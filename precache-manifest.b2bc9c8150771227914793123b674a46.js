c4f84eb31dd689feadf46f8a4ce4858102f0f521
NofarPeled <Nofarpeled4@gmail.com>
2020-05-10 10:11:55 +0300

    Updates

---
+++ precache-manifest.b2bc9c8150771227914793123b674a46.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87bb168043b245077301ba6e35464725",
+    "url": "/Nofar-Peled-Portfolio/index.html"
+  },
+  {
+    "revision": "ba76b951d73bc3f029c4",
+    "url": "/Nofar-Peled-Portfolio/static/css/main.f6ab0be5.chunk.css"
+  },
+  {
+    "revision": "fdcba4bfa3d7dced3f48",
+    "url": "/Nofar-Peled-Portfolio/static/js/2.bd9680d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Nofar-Peled-Portfolio/static/js/2.bd9680d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba76b951d73bc3f029c4",
+    "url": "/Nofar-Peled-Portfolio/static/js/main.c8401432.chunk.js"
+  },
+  {
+    "revision": "9291fa08a34ad8ff4801",
+    "url": "/Nofar-Peled-Portfolio/static/js/runtime-main.f71720e0.js"
+  },
+  {
+    "revision": "7582e823ef0d702969ea0cce9afb326d",
+    "url": "/Nofar-Peled-Portfolio/static/media/Lato-Italic.7582e823.ttf"
+  },
+  {
+    "revision": "2fe27d9d10cdfccb1baef28a45d5ba90",
+    "url": "/Nofar-Peled-Portfolio/static/media/Lato-Light.2fe27d9d.ttf"
+  },
+  {
+    "revision": "4d80ac573c53d192dafd99fdd6aa01e9",
+    "url": "/Nofar-Peled-Portfolio/static/media/Lato-LightItalic.4d80ac57.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/Nofar-Peled-Portfolio/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "7489a412c5336d85b93ec5a7253c2a0f",
+    "url": "/Nofar-Peled-Portfolio/static/media/backend.7489a412.png"
+  },
+  {
+    "revision": "a9b52655deea2e319f7ace629d0ad46b",
+    "url": "/Nofar-Peled-Portfolio/static/media/covid-19.a9b52655.png"
+  },
+  {
+    "revision": "3011de119eb75d67734656b25124f7b4",
+    "url": "/Nofar-Peled-Portfolio/static/media/database.3011de11.png"
+  },
+  {
+    "revision": "ae7dad5ad0fd8e861a5e6dfd72e8a580",
+    "url": "/Nofar-Peled-Portfolio/static/media/design.ae7dad5a.png"
+  },
+  {
+    "revision": "bf0267de28d4c0a25033ee7275064547",
+    "url": "/Nofar-Peled-Portfolio/static/media/forest.bf0267de.jpg"
+  },
+  {
+    "revision": "35d8d625d9eac45b2d934fee110760e3",
+    "url": "/Nofar-Peled-Portfolio/static/media/frontend.35d8d625.png"
+  },
+  {
+    "revision": "718eefd114124f598f16e8dcc0b552d6",
+    "url": "/Nofar-Peled-Portfolio/static/media/logo.718eefd1.png"
+  },
+  {
+    "revision": "50ec3fe85f05e24238166d2c42dabfa5",
+    "url": "/Nofar-Peled-Portfolio/static/media/portfolio.50ec3fe8.png"
+  },
+  {
+    "revision": "ca463bd892de83aa51ed9d2b330bd99b",
+    "url": "/Nofar-Peled-Portfolio/static/media/responsive.ca463bd8.png"
+  },
+  {
+    "revision": "e2e07eb348737d4d461ef9e3cddc81bb",
+    "url": "/Nofar-Peled-Portfolio/static/media/rock-river.e2e07eb3.jpg"
+  },
+  {
+    "revision": "df6ad93c7d09db68b0aa56b0d5529279",
+    "url": "/Nofar-Peled-Portfolio/static/media/sea-side.df6ad93c.jpg"
+  },
+  {
+    "revision": "970410dac6526f37c7d50baeacb962c8",
+    "url": "/Nofar-Peled-Portfolio/static/media/security.970410da.png"
+  },
+  {
+    "revision": "957ca7a6ace371c6890fe17e8d7734a5",
+    "url": "/Nofar-Peled-Portfolio/static/media/skyscraper.957ca7a6.jpg"
+  }
+]);
